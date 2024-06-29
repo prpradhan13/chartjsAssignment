86ed59877ea21a4a56f19f502768a4ee94264f41
Pritiranjan Pradhan <ppritiranjan7@gmail.com>
2024-06-29 16:26:02 +0530

    Update

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 // config env
 dotenv.config();
@@ -37,20 +35,11 @@ const DataSchema = new mongoose.Schema({
 // Define a model
 const Data = mongoose.model('Data', DataSchema);
 
-// ES module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, './frontend/dist')));
-
-app.use('*', function(req, res){
-  res.sendFile(path.join(__dirname, './frontend/dist/index.html'))
-});
 
 app.get('/api/data', async (req, res) => {
     try {
